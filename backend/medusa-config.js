e8805566d7caa8337e72b160510088370c5c8325
Philip Korsholm <philip.korsholm@hotmail.com>
2021-11-24 16:22:05 +0100

    update medusa-config

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -6,7 +6,7 @@ const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-storeas";
+  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
